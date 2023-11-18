3ff58b589594ec5f6c61b64c2809aa35a2ee9ddc
texnoparkyu <dilbekergashev6@gmail.com>
2023-11-18 15:30:00 +0500

    yangilandi

--- main.js
+++ main.js
@@ -13,40 +13,71 @@ console.log(urta);
 //matn end
 
 //qushish start
-let qushish =   12
-let qushish2 =  18
-let qushish3 =  34
-let qushish4 =  45
-let qushish5 =  89
-console.log(qushish+qushish2+qushish3+qushish4+qushish5);
+let qushish = 12
+let qushish2 = 18
+let qushish3 = 34
+let qushish4 = 45
+let qushish5 = 89
+console.log(qushish);
+console.log(qushish2);
+console.log(qushish3);
+console.log(qushish4);
+console.log(qushish5);
 //qushish end
 //minus start
 let ayrish = 23847748329478
 let ayrish2 = 23243
-let ayrish3 =345
-let ayrish4= 234
-let ayrish5=234 
-console.log(ayrish-ayrish2-ayrish3-ayrish4-ayrish5);
+let ayrish3 = 345
+let ayrish4 = 234
+let ayrish5 = 234
+console.log(ayrish);
+console.log(ayrish2);
+console.log(ayrish3);
+console.log(ayrish4);
+console.log(ayrish5);
 //minus end
 //kupaytiruv start
-let kupaytiruv=19
-let kupaytiruv1=12
-let kupaytiruv2=58
-let kupaytiruv3=23
-let kupaytiruv4=23
-let natija = kupaytiruv*kupaytiruv2*kupaytiruv3
+let kupaytiruv = 19
+let kupaytiruv1 = 12
+let kupaytiruv2 = 58
+let kupaytiruv3 = 23
+let kupaytiruv4 = 23
+let natija = kupaytiruv * kupaytiruv2 * kupaytiruv3
 console.log(natija)
 //kupaytiruv end
 //bulish start
-let bulish=13498593
-let bulish1=27
-let bulish2=23
-let bulish3=48
-let bulish4=23
-console.log(bulish/bulish1/bulish2/bulish3/bulish4);
+let bulish = 13498593
+let bulish1 = 27
+let bulish2 = 23
+let bulish3 = 48
+let bulish4 = 23
+console.log(bulish );
+console.log(bulish1);
+console.log(bulish2);
+console.log(bulish3);
+console.log(bulish4);
 //bulish end
-let ikki=2
-let bir=1
-let natijam=ikki-bir
-console.log (natijam) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let ikki = 2
+let bir = 1
+let natijam = ikki - bir
+console.log(natijam)
 
