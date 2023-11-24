f22478bbf9b16f01074803f0a79047af7a96fab7
папка <you@example.com>
2023-11-24 15:30:31 +0500

    2 dars vazifasi bo'ldi

--- main.js
+++ main.js
@@ -1,83 +1,96 @@
 //matn start
-let salomlashish = "Qonday sen e"
-let Orzu = "men nixoyat jsga O'tdim"
-let katta = "katta"
-let kichik = "kichik"
-let urta = "O'rtacha"
+// let salomlashish = "Qonday sen e"
+// let Orzu = "men nixoyat jsga O'tdim"
+// let katta = "katta"
+// let kichik = "kichik"
+// let urta = "O'rtacha"
 
-console.log(salomlashish);
-console.log(Orzu);
-console.log(katta);
-console.log(kichik);
-console.log(urta);
+// console.log(salomlashish);
+// console.log(Orzu);
+// console.log(katta);
+// console.log(kichik);
+// console.log(urta);
 //matn end
 
 //qushish start
-let qushish = 12
-let qushish2 = 18
-let qushish3 = 34
-let qushish4 = 45
-let qushish5 = 89
-console.log(qushish);
-console.log(qushish2);
-console.log(qushish3);
-console.log(qushish4);
-console.log(qushish5);
+// let qushish = 12
+// let qushish2 = 18
+// let qushish3 = 34
+// let qushish4 = 45
+// let qushish5 = 89
+// console.log(qushish);
+// console.log(qushish2);
+// console.log(qushish3);
+// console.log(qushish4);
+// console.log(qushish5);
 //qushish end
 //minus start
-let ayrish = 23847748329478
-let ayrish2 = 23243
-let ayrish3 = 345
-let ayrish4 = 234
-let ayrish5 = 234
-console.log(ayrish);
-console.log(ayrish2);
-console.log(ayrish3);
-console.log(ayrish4);
-console.log(ayrish5);
+// let ayrish = 23847748329478
+// let ayrish2 = 23243
+// let ayrish3 = 345
+// let ayrish4 = 234
+// let ayrish5 = 234
+// console.log(ayrish);
+// console.log(ayrish2);
+// console.log(ayrish3);
+// console.log(ayrish4);
+// console.log(ayrish5);
 //minus end
 //kupaytiruv start
-let kupaytiruv = 19
-let kupaytiruv1 = 12
-let kupaytiruv2 = 58
-let kupaytiruv3 = 23
-let kupaytiruv4 = 23
-let natija = kupaytiruv * kupaytiruv2 * kupaytiruv3
-console.log(natija)
+// let kupaytiruv = 19
+// let kupaytiruv1 = 12
+// let kupaytiruv2 = 58
+// let kupaytiruv3 = 23
+// let kupaytiruv4 = 23
+// let natija = kupaytiruv * kupaytiruv2 * kupaytiruv3
+// console.log(natija)
 //kupaytiruv end
 //bulish start
-let bulish = 13498593
-let bulish1 = 27
-let bulish2 = 23
-let bulish3 = 48
-let bulish4 = 23
-console.log(bulish );
-console.log(bulish1);
-console.log(bulish2);
-console.log(bulish3);
-console.log(bulish4);
+// let bulish = 13498593
+// let bulish1 = 27
+// let bulish2 = 23
+// let bulish3 = 48
+// let bulish4 = 23
+// console.log(bulish);
+// console.log(bulish1);
+// console.log(bulish2);
+// console.log(bulish3);
+// console.log(bulish4);
 //bulish end
 
 
+// let ikki = 2
+// let bir = 1
+// let natijam = ikki - bir
+// console.log(natijam)
+// dars 3
 
 
+let hafta = ["Dushanba", "seshamba ", "chorshanba", "payshanba", "juma", "shamba"]
+hafta.unshift("bir", "ikki", "uch")
+console.log(hafta);
 
 
+let kitoblar = ["fizika", 'algebra', 'rustili', 'ingliz tili']
+kitoblar.push("fransuz", "yapon", "nemis")
+console.log(kitoblar);
+
+let uy_xayvonlar = ["sigir", "ot", "eshak", "qo'y", 'it']
+uy_xayvonlar.pop()
+console.log(uy_xayvonlar);
+
+let ism = ["temur", 'aziz', 'nodir', 'jaxongir', 'zarux']
+ism.shift()
+console.log(ism);
+
+let davlat = ["uzbekiston", 'tojikiston', 'yaponya', 'koreya', 'hindiston']
+davlat.splice(0, 1)
+console.log(davlat);
+
+let para = ["usd", 'jpy', 'eur', 'uzs', 'gpb']
+console.log(para.includes("jpy"));
 
 
-
-
-
-
-
-
-
-
-
-
-
-let ikki = 2
-let bir = 1
-let natijam = ikki - bir
-console.log(natijam)
+let ichimlik = ['pepsi', 'fanta', 'cola', 'bliss', 'flesh']
+console.log(ichimlik.indexOf("cola"));
 
